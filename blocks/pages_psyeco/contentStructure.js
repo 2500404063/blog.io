133b2313a6a89e5bdb30afdb4f8c4d5e8a7c1be8
Felix <859317053@qq.com>
2021-07-24 15:10:44 +0800

    Add files via upload

--- blocks/pages_psyeco/contentStructure.js
+++ blocks/pages_psyeco/contentStructure.js
@@ -2,19 +2,19 @@ var contentArray = {
     "Default Page": "./pages_psyeco/default/default.md",
     "生活哲学": {
         "为什么要哲学?": "./pages_psyeco/life-psy/1.md",
-        "心理学": {},
-        "论性格": {
+        "性格": {
             "HSP高敏感人群": "./pages_psyeco/life-psy/personality/1.md"
         },
-        "论友情": {},
-        "论爱情": {},
-        "论事业": {
+        "常规思考": {
             "工作-心态-健康": "./pages_psyeco/life-psy/cause/1.md",
             "过度思考Overthink": "./pages_psyeco/life-psy/cause/2.md",
-            "不要犹豫,Just Do It": "./pages_psyeco/life-psy/cause/3.md"
+            "不要犹豫,Just Do It": "./pages_psyeco/life-psy/cause/3.md",
+            "什么是充实的人生": "./pages_psyeco/life-psy/cause/4.md",
         },
-        "论学习": {
-            "高效学习": "./pages_psyeco/life-psy/study/1.md"
+        "学习与提高": {
+            "高效学习": "./pages_psyeco/life-psy/study/1.md",
+            "知识有什么用": "./pages_psyeco/life-psy/study/2.md",
+            "连续时间的重要性": "./pages_psyeco/life-psy/study/3.md",
         }
     },
     "经济学": {},
