b8509e61b67a2135e23ad5fad274b31a1e1112dc
Felix <859317053@qq.com>
2021-08-17 17:41:32 +0800

    Add files via upload

--- blocks/pages_hardware/contentStructure.js
+++ blocks/pages_hardware/contentStructure.js
@@ -1,13 +1,14 @@
 var contentArray = {
     "Default Page": "./pages_hardware/default/default.md",
-    "Html": {
-        "Elements": {
-            "item1_1_1": "./pages_hardware/hello/main.md"
-        },
-        "item1_1": "./pages_hardware/hello/main.md",
+    "从C51看MCU": {
+        "基本概念": "./pages_hardware/mcustudy/1.md",
+        "GPIO": "./pages_hardware/mcustudy/2.md",
+        "Clock": "./pages_hardware/mcustudy/3.md",
+        "Timer": "./pages_hardware/mcustudy/4.md",
+        "UART": "./pages_hardware/mcustudy/5.md",
+        "EEPROM": "./pages_hardware/mcustudy/6.md",
+        "比较器": "./pages_hardware/mcustudy/7.md",
+        "ADC模数转换": "./pages_hardware/mcustudy/8.md",
+        "PCA/CCP": "./pages_hardware/mcustudy/9.md"
     },
-    "list2": {
-        "item2_1": "./pages_hardware/hello/main.md",
-        "item2_2": "./pages_hardware/hello/main.md"
-    }
 };
